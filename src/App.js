c9ab2c36be16fd10140aa969b955881f51a6b09a
Angelo Padron <padron891@gmail.com>
2020-07-15 15:34:29 -0300

    update App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router, Route, Switch, BrowserRouter } from 'react-router-dom'
+import Landing from './Landing'
+import Game from './Game'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Landing}/>
+                <Route exact path='/game' component={Game}/>
+            </Switch>
+        </BrowserRouter>
+    )
 }
-
-export default App;
